02b083571c4bbe2081a80503f9faa0aad63690f8
Timofey Peshin <timoffex@gmail.com>
2020-02-15 20:17:58 -0800

    Implemented Positioned.hs

---
+++ src/BattleAnts/World/Positioned.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | Defines the 'Positioned' class for data with positions.
+module BattleAnts.World.Positioned
+    ( Positioned (positionOf)
+    , WithPosition (WithPosition)
+    , HasPosition (position)
+    , HasDatum (datum)
+    ) where
+
+import Control.Lens
+import Data.Grid
+
+-- | Class for data with a readable position.
+class Positioned a where
+  positionOf :: a -> GridPosition
+
+-- | Adds a position to a datum.
+data WithPosition d = WithPosition
+  { _withPositionPosition :: GridPosition
+  , _withPositionDatum :: d
+  }
+makeFields ''WithPosition
+
+instance Positioned (WithPosition d) where
+  positionOf = view position
