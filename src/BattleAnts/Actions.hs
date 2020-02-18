7859000aed3ca904e2ff4b27c10716f29bced76f
Timofey Peshin <timoffex@gmail.com>
2020-02-17 22:36:23 -0800

    Wrote Actions.hs

---
+++ src/BattleAnts/Actions.hs
@@ -0,0 +1,11 @@
+-- | Defines the action system.
+module BattleAnts.Actions
+    ( resolveActions
+    , ActionData (..)
+    , MoveData (..)
+    , ActionResolver
+    ) where
+
+import BattleAnts.Actions.Action
+import BattleAnts.Actions.ActionResolver
+import BattleAnts.Actions.MoveAction
